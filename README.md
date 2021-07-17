f41f6d20af76efac42390cec5d105bbcd6fef224
César Nóvoa <themotokar@gmail.com>
2021-07-18 01:56:33 +0200

    card npx personalization and index.html resume

--- README.md
+++ README.md
@@ -1,16 +1,54 @@
-### Hi there 👋
+# **Hola, Im César Nóvoa**  <img src='img/hi.gif' width='20'>
 
-<!--
-**rasekdk/rasekdk** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+_Web Developer at [ingenyus](https://ingenyus.es) <img src='img/current-job.png' width='10'>_
 
-Here are some ideas to get you started:
+---
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+[![Linkedin](https://img.shields.io/badge/-Linkedin-blue?style=flat-square&logo=Linkedin&logoColor=white)](https://www.linkedin.com/in/rasekdk/)
+[![Linkedin](https://img.shields.io/badge/-Twitter-blue?style=flat-square&logo=twitter&logoColor=white)](https://www.twitter.com/rasekdark/)
+
+
+---
+
+👇 Contact with me through the console
+
+```
+npx rasek
+```
+
+_Created by **[anmol098](https://github.com/anmol098)**_
+
+---
+
+## Something about me 
+
+``` js
+let rasek = {
+    code: ["Javascript", "Phyton", "PHP", "Typescript"],
+    askMeAbout: ["web dev", "web design", "graphic desing", "gaming"],
+    studies: ["Graphic Design", "Web Development"],
+    technologies: {
+        backEnd: {
+            js: ["Node", "Express"],
+        },
+        frontEnd: {
+            js: ["React", "React Native", "Vue"]
+        },
+        devOps: ["Docker"],
+        dataBases: ["MySql"],
+    },
+    futureLook: "Become a mix between my both parts, the graphic designer and the web developer. Some call it UX engineer.",
+
+}
+```
+**Feel free to say hi** if you want to know more about me, or just talk 😊
+
+---
+
+### That's all Folks...  For now!!! 🐖
+
+- You can contact with me through [Linkedin](https://linkedin.com/in/rasekdk) or [Email](mailto:themotokar@gmail.com)
+
+---
+
+⌨️ with ❤️ by [César Nóvoa](https://github.com/rasekdk) 😊
--- rasek/card.js
+++ rasek/card.js
@@ -1,3 +1,104 @@
 #!/usr/bin/env node
 
-console.log('Hello World!');
+'use strict'
+
+const boxen = require('boxen');
+const chalk = require('chalk');
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:themotokar@gmail.com");
+                    console.log("\nDone, see you soon at inbox.\n" + "\nJust in case: themotokar@gmail.com \n");
+                }
+            },
+            {
+                name: `Download my ${chalk.hex('#fcba03').bold("Resume")}?`,
+                value: () => {
+                    open('')
+                }
+            },
+            {
+                name: 'Just quit',
+                value: () => {
+                    console.log(chalk.hex('#fcf003').bold("That's all folks!.\n"));
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: '                   ' + chalk.hex('#fcba03').bold("César Nóvoa Fernández"),
+            
+    job: chalk.white("Web Developer at ") + chalk.hex("#ab56e9").bold("ingenyus*"),
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("rasekdk"),
+    github: chalk.gray("https://github.com/") + chalk.green("rasekdk"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("rasekdk"),
+    web: chalk.cyan("Work in Progress"),
+    npx: chalk.red("npx") + " " + chalk.white("rasek"),
+
+    
+    labelWork: chalk.white.bold("       Work:") + ' ',
+    labelGitHub: chalk.white.bold("     GitHub:") + ' ',
+    labelTwitter: chalk.white.bold("    Twitter:") + ' ',
+    labelLinkedIn: chalk.white.bold("   LinkedIn:") + ' ',
+    labelWeb: chalk.white.bold("        Web:") + ' ',
+    labelCard: chalk.white.bold("       Card:") + ' '
+    
+}
+
+
+const me = boxen(
+    [
+        data.name,
+        ``,
+        data.labelWork + data.job,
+        ``,
+        data.labelGitHub + data.github,
+        data.labelLinkedIn + data.linkedin,
+        data.labelTwitter + data.twitter,
+        data.labelWeb + data.web,
+        ``,
+        data.labelCard + data.npx,
+        ``,
+        `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    ].join('\n'),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: 'single',
+        borderColor: 'green'
+    }
+);
+
+console.log(me);
+
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
