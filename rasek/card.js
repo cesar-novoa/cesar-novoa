67572484ef9ada9c1c5484f93267cb68d7756a30
César Nóvoa <themotokar@gmail.com>
2021-07-18 02:31:21 +0200

    Ended card npx

--- rasek/card.js
+++ rasek/card.js
@@ -4,41 +4,36 @@
 
 const boxen = require('boxen');
 const chalk = require('chalk');
-const inquirer = require("inquirer");
-const clear = require("clear");
-const open = require("open");
-const fs = require('fs');
-const request = require('request');
-const path = require('path');
-const ora = require('ora');
-const cliSpinners = require('cli-spinners');
+const inquirer = require('inquirer');
+const clear = require('clear');
+const open = require('open');
 clear();
 
 const prompt = inquirer.createPromptModule();
 
 const questions = [
     {
-        type: "list",
-        name: "action",
-        message: "What you want to do?",
+        type: 'list',
+        name: 'action',
+        message: 'What you want to do?',
         choices: [
             {
-                name: `Send me an ${chalk.green.bold("email")}?`,
+                name: `Send an ${chalk.green.bold('email')}`,
                 value: () => {
-                    open("mailto:themotokar@gmail.com");
-                    console.log("\nDone, see you soon at inbox.\n" + "\nJust in case: themotokar@gmail.com \n");
+                    open('mailto:themotokar@gmail.com');
+                    console.log('\nDone, see you soon at inbox.\n' + '\nJust in case: themotokar@gmail.com \n');
                 }
             },
             {
-                name: `Download my ${chalk.hex('#fcba03').bold("Resume")}?`,
+                name: `Open my  ${chalk.hex('#fcba03').bold('Resume')}`,
                 value: () => {
-                    open('')
+                    open('https://rasekdk.github.io/rasekdk/')
                 }
             },
             {
                 name: 'Just quit',
                 value: () => {
-                    console.log(chalk.hex('#fcf003').bold("That's all folks!.\n"));
+                    console.log(chalk.hex('#fcf003').bold('That\'s all folks!.\n'));
                 }
             }
         ]
@@ -46,22 +41,22 @@ const questions = [
 ];
 
 const data = {
-    name: '                   ' + chalk.hex('#fcba03').bold("César Nóvoa Fernández"),
+    name: '              ' + chalk.hex('#fcba03').bold('César Nóvoa Fernández') + '              ',
             
-    job: chalk.white("Web Developer at ") + chalk.hex("#ab56e9").bold("ingenyus*"),
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("rasekdk"),
-    github: chalk.gray("https://github.com/") + chalk.green("rasekdk"),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("rasekdk"),
-    web: chalk.cyan("Work in Progress"),
-    npx: chalk.red("npx") + " " + chalk.white("rasek"),
+    job: chalk.white('Web Developer at ') + chalk.hex('#ab56e9').bold('ingenyus*'),
+    twitter: chalk.gray('https://twitter.com/') + chalk.hex('#1da1f2').bold('rasekdk'),
+    github: chalk.gray('https://github.com/') + chalk.hex('#0550ae').bold('rasekdk'),
+    linkedin: chalk.gray('https://linkedin.com/in/') + chalk.hex('#0A66C2').bold('rasekdk'),
+    web: chalk.cyan('Work in Progress'),
+    npx: chalk.red('npx') + ' ' + chalk.white('rasek'),
 
     
-    labelWork: chalk.white.bold("       Work:") + ' ',
-    labelGitHub: chalk.white.bold("     GitHub:") + ' ',
-    labelTwitter: chalk.white.bold("    Twitter:") + ' ',
-    labelLinkedIn: chalk.white.bold("   LinkedIn:") + ' ',
-    labelWeb: chalk.white.bold("        Web:") + ' ',
-    labelCard: chalk.white.bold("       Card:") + ' '
+    labelWork: chalk.white.bold('       Work:') + ' ',
+    labelGitHub: chalk.white.bold('     GitHub:') + ' ',
+    labelTwitter: chalk.white.bold('    Twitter:') + ' ',
+    labelLinkedIn: chalk.white.bold('   LinkedIn:') + ' ',
+    labelWeb: chalk.white.bold('        Web:') + ' ',
+    labelCard: chalk.white.bold('       Card:') + ' '
     
 }
 
@@ -79,14 +74,17 @@ const me = boxen(
         ``,
         data.labelCard + data.npx,
         ``,
-        `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+        '            ' + 'I\'m open to new Adventures' + '           ', 
+        '',
+        '        ' + 'If you want to know more about me,' + '       ',
+        '           ' + 'fell free to contact with me' + '          ',
     ].join('\n'),
     {
         margin: 1,
         float: 'center',
         padding: 1,
         borderStyle: 'single',
-        borderColor: 'green'
+        borderColor: 'yellow'
     }
 );
 
@@ -94,10 +92,10 @@ console.log(me);
 
 const tip = [
     `Tip: Try ${chalk.cyanBright.bold(
-        "cmd/ctrl + click"
+        'cmd/ctrl + click'
     )} on the links above`,
     '',
-].join("\n");
+].join('\n');
 
 console.log(tip);
 
